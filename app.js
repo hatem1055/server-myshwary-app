12a29a48497875e2d6d904f2c51ca8631f9dd604
hatem1055 <hatemmostafa31@gmail.com>
2020-10-21 00:34:38 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express'),
+    app = express(),
+    path = require('path')
+let port = process.env.PORT || 3000
+
+// //mongoose setup test
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb+srv://hatem1055:hatemmostafa@cluster0.tfqdd.mongodb.net/test' ,{
+//     useNewUrlParser:true,
+//     useCreateIndex:true,
+//     useUnifiedTopology: true 
+// })
+// const devSchema = mongoose.Schema({
+//     name:{
+//         type: String
+//     }
+// })
+
+// const devModle = mongoose.model('Developer',devSchema)
+
+// //test endpoint
+// app.get('/test',async (req, res)=>{
+//     let newDev = new devModle({
+//         name:'hatem'
+//     })
+//     await newDev.save()
+//     res.send(newDev.name);
+// })
+
+//runing the app
+app.listen(port, () => {
+    console.log('server is up on port:' + port)
+});
